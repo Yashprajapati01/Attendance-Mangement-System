2efe08a41d43ff47bdf63e789b4c7c8b7cf7ca01
Yash <Prajapatiyash0011@gmail.com>
2024-07-18 23:03:08 +0530

    commit

--- lib/Dashboard Admin/Screens/ProgramScreen.dart
+++ lib/Dashboard Admin/Screens/ProgramScreen.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
@@ -23,47 +24,98 @@ class ProgrammeScreen extends StatelessWidget {
       appBar: AppBar(
         title: Text('Programme: $programmeName'),
       ),
-      body: Column(
-        children: [
-          ElevatedButton(
-            onPressed: () => _createBranch(context),
-            child: Text('Create Branch'),
-          ),
-          Expanded(
-            child: StreamBuilder<QuerySnapshot>(
-              stream: FirebaseFirestore.instance
-                  .collection('branches')
-                  .where('programmeId', isEqualTo: programmeId)
-                  .snapshots(),
-              builder: (context, snapshot) {
-                if (!snapshot.hasData) {
-                  return CircularProgressIndicator();
-                }
-                var branches = snapshot.data!.docs;
-                return ListView.builder(
-                  itemCount: branches.length,
-                  itemBuilder: (context, index) {
-                    var branch = branches[index];
-                    return ListTile(
-                      title: Text(branch['name']),
-                      onTap: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                            builder: (context) => BranchScreen(
-                              branchId: branch.id,
-                              branchName: branch['name'],
-                            ),
-                          ),
-                        );
-                      },
-                    );
-                  },
-                );
-              },
+      body: Padding(
+        padding: const EdgeInsets.all(14.0),
+        child: Column(
+          children: [
+            GestureDetector(
+              onTap: () =>  _createBranch(context),
+              child: const Card(
+                color: Color(0xff4B70F5),
+                child: Padding(
+                  padding: EdgeInsets.all(16.0), // padding inside the Card
+                  child: Column(
+                    children: [
+                      Row(
+                        children: [Text('Add',
+                          style: TextStyle(
+                              fontSize: 20
+                          ),)],
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text('Branch Here',
+                            style: TextStyle(
+                                fontSize: 20
+                            ),),
+                          Icon(Icons.add),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ),
             ),
-          ),
-        ],
+            const SizedBox(height: 20),
+            const Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Text('Branch List :',
+                  style: TextStyle(
+                      fontSize: 16
+                  ),),
+              ],
+            ),
+            Expanded(
+              child: StreamBuilder<QuerySnapshot>(
+                stream: FirebaseFirestore.instance
+                    .collection('branches')
+                    .where('programmeId', isEqualTo: programmeId)
+                    .snapshots(),
+                builder: (context, snapshot) {
+                  if (!snapshot.hasData) {
+                    return CircularProgressIndicator();
+                  }
+                  var branches = snapshot.data!.docs;
+                  return ListView.builder(
+                    itemCount: branches.length,
+                    itemBuilder: (context, index) {
+                      var branch = branches[index];
+                      return Container(
+                        margin: EdgeInsets.symmetric(vertical: 5),
+                        child: Card(
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(20),
+                          ),
+                          child: ListTile(
+                            title: Text(branch['name']),
+                            leading: Text('Branch :',
+                              style: TextStyle(
+                                  fontSize: 16
+                              ),),
+                            trailing: Icon(CupertinoIcons.forward),
+                            onTap: () {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (context) => BranchScreen(
+                                    branchId: branch.id,
+                                    branchName: branch['name'],
+                                  ),
+                                ),
+                              );
+                            },
+                          ),
+                        ),
+                      );
+                    },
+                  );
+                },
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
